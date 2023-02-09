95125da2e13f2070e9e102289eae54cddffeb9f7
Amir Mohammad Jahangirzad <sudoeruser@gmail.com>
2023-02-09 20:07:32 +0330

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sstream>
+#include <iostream>
+
+#include "linenoise.h"
+#include "debugger.hpp"
+
+
+
+
+
+void execute_debugee (const std::string& prog_name) {
+    if (ptrace(PTRACE_TRACEME, 0, 0, 0) < 0) {
+        std::cerr << "Error in ptrace\n";
+        return;
+    }
+    execl(prog_name.c_str(), prog_name.c_str(), nullptr);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Program name not specified" << std::endl;
+        return -1;
+    }
+
+    auto prog = argv[1];
+
+    auto pid = fork();
+    if (pid == 0) {
+        //child
+        execute_debugee(prog);
+
+    }
+    else if (pid >= 1)  {
+        //parent
+        std::cout << "Started debugging process " << pid << '\n';
+        debugger dbg{prog, pid};
+        dbg.run();
+    }
+}
+
